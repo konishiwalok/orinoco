01f347ccd0598e27a047332471cb1dc9a0e2f30b
angie <sophie-girard91@outlook.fr>
2021-06-22 15:22:08 +0200

    tres pag funcionan, arregalr produits, y comienzo de code le panier

--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -5,11 +5,15 @@ const url = 'http://localhost:3000/api/cameras';
 const products = document.getElementById('products'); // llama donde va a ir la info 
 const produitItems = document.getElementById('produit-card').content; // llamado del template por id
 const fragment = document.createDocumentFragment(); // agrega el fragmento de codigo que se quiere mostrar
+let carrito = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
 })
 
+products.addEventListener('click' , e => {
+ addCarrito(e)
+ })
 
 const fetchData = async () => {
 
@@ -32,13 +36,36 @@ const imprimirCards = data => {
         produitItems.querySelector('p').textContent = product.description;
         produitItems.querySelector('img').setAttribute('src', product.imageUrl);
         produitItems.querySelector('.price-camera').textContent = product.price;
-      
-
-
+        produitItems.querySelector('.btn-dark').dataset.id = product.id;        
+        
         const clone = produitItems.cloneNode(true);
+        
         fragment.appendChild(clone);
 
     })
     products.appendChild(fragment);
 }
-/* PAGE PRODUIT */
+// esto es para agregar el boton y su id
+const addCarrito = e => {
+    // console.log(e.target)
+    // console.log (e.target.classList.contains('btn-dark'))
+    if(e.target.classList.contains('btn-dark')) {
+    setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+const setCarrito = objeto =>{
+    // console.log(objeto)
+    const producto = { 
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: objeto.querySelector('h3').textContent,
+        price: objeto.querySelector('p').textContent,
+        cantidad:1
+    } 
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+    carrito[producto.id] = {...producto}
+     console.log(carrito)
+    
+}
