d4013332ef33adc7e33d23ffcfd42e51cc79f946
angie <sophie-girard91@outlook.fr>
2021-06-29 19:56:21 +0200

    pages modifies deco + meta + responsive

--- frontend/js/footer.js
+++ frontend/js/footer.js
@@ -2,20 +2,16 @@
 
 const footer = `
 <div id="footer" class="py-5">
-    <div class="col">
-        <h3 class="text-center" style="color: var(--bs-blue)">suivez nous sur</h3>
+    <div class="col py-3">
+        <h3 class="text-center " style="color: var(--bs-blue)">Contactez Nous</h3>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-2"><a href="#">Facebook</a></div>
-        <div class="col-2"><a href="#">twitter</a></div>
-        <div class="col-2"><a href="#">pinterest</a></div>
-        <div class="col-1"><a href="#">youtube </a></div>
+    <div class="row justify-content-center py-3">
+        <div class="col-2  stilo700 h4" borrar  borrar><a href="#">Facebook</a></div>
+        <div class="col-2  stilo700 h4" borrar><a href="#">Twitter</a></div>
+        <div class="col-2  stilo700 h4" borrar><a href="#">Pinterest</a></div>
+        <div class="col-1  stilo700 h4" borrar><a href="mailto:orinoco-cameras@yahoo.com">Email</a>
     </div>
-    <div class="col">
-    <h3 class="text-center" style="color: var(--bs-blue)">Contactez Nous</h3>
-</div>
-
-    <div class="row"></div>
+   
 </div>
 `
 
