052571ef2ce1632e242c105ef6afc2a3bb519a98
angie <sophie-girard91@outlook.fr>
2021-07-01 17:59:08 +0200

    proYEI

--- frontend/js/recap.js
+++ frontend/js/recap.js
@@ -1,5 +1,45 @@
-;(() => {
-    const orderId = new URL(location.href).searchParams.get('orderId') || 'ERREUR'
-    document.getElementById('commandId').textContent = orderId
-  })()
+
+
+
+// ;(() => {
+//     const orderId = new URL(location.href).searchParams.get('orderId') || 'ERREUR'
+//     document.getElementById('commandId').textContent = orderId
+//   })()
  
+//   function sendOrder() {
+//     const firstname = document.getElementById('firstname').value
+//     const lastname = document.getElementById('lastname').value
+//     const adress = document.getElementById('adress').value
+//     const zipcode = document.getElementById('zipcode').value
+//     const email = document.getElementById('email').value
+//     const city = document.getElementById('city').value
+    
+//     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+//     const zipcodeRegex = /[0-9]{5}(-[0-9]{4})?/
+  
+//     if (!(
+//       firstname.length > 1
+//       && lastname.length > 1
+//       && emailRegex.test(email)
+//       && adress.length > 6
+//       && zipcodeRegex.test(zipcode)
+//       && city.length > 1
+//     )) {
+//       alert("Veuillez remplir les champs correctements avant de procéder au paiement")
+//       return
+      
+//     }
+    
+// const showAllCards = data => {
+//   data.forEach( product => {
+
+//       cardItems.querySelector('h5').textContent = product.name;
+//       cardItems.querySelector('p').textContent = product.description;
+//       cardItems.querySelector('img').setAttribute('src', product.imageUrl);
+//       cardItems.querySelector('a').setAttribute('href',  `product.html?id=${product._id}`);
+
+//       const clone = cardItems.cloneNode(true);
+//       fragment.appendChild(clone);
+//   })
+//   items.appendChild(fragment);
+// }
--- frontend/js/validation.js
+++ frontend/js/validation.js
@@ -1,7 +1,53 @@
 
-  // Input validity
-  watchValidity(document.getElementById('firstname'), (e) => e.target.value.length > 1)
-  watchValidity(document.getElementById('lastname'), (e) => e.target.value.length > 1)
+
+
+
+
+// function enviarFormulario(){
+//   console.log("enviando formulario")
+
+//   let mensajesError =[]; 
+
+//   if (nom.value === null ||nom.value === ''){
+//     meansajError.push
+//   }
+
+//   return false
+// }
+
+
+
+// // Input validity
+// let nom= document.getElementById('fistname');
+// let lastname= document.getElementById('lastname');
+// let email= document.getElementById('email');
+// let adress= document.getElementById('adress');
+// let zipcode= document.getElementById('zipcode');
+// let city= document.getElementById('city');
+
+// const formulario = document.getElementById('user-form');
+// const submitBoton = document.getElementById('confirmPurchase');
+
+
+// document.querySelectorAll('.form-box').forEach((box)) =>{
+
+  //   const boxInput = box.querySelector('input');
+
+//   boxInput.addEventListener('keydown',(event) => {
+  
+//     console.log ('input $ {boxInput.firstname} value ' , boxInput.value);
+
+//   });
+
+// Input validity
+const expresiones = {
+  	name: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+  	lastname: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+
+  }
+
+watchValidity(document.getElementById('firstname'), (e) => e.target.value.length > 1)
+watchValidity(document.getElementById('lastname'), (e) => e.target.value.length > 1)
   watchValidity(document.getElementById('email'), (e) => {
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     return emailRegex.test(e.target.value)
@@ -12,7 +58,6 @@
     return zipcodeRegex.test(e.target.value)
   })
   watchValidity(document.getElementById('city'), (e) => e.target.value.length > 1)
-
 
 function watchValidity(elt, condition) {
   elt.oninput = (e) => {
@@ -36,14 +81,14 @@ function validInputElt(elt) {
 }
 
 function invalidInputElt(elt) {
-  elt.style.border = 'solid 2px red'
+  elt.style.border = 'solid 1px red'
   elt.style.boxShadow = 'rgba(128, 0, 0, 0.4) 0px 0px 4px'
 }
 
-// function neutralInputElt(elt) {
-//   elt.style.border = ''
-//   elt.style.boxShadow = ''
-// }
+function neutralInputElt(elt) {
+  elt.style.border = ''
+  elt.style.boxShadow = ''
+}
 
 function sendOrder() {
   const firstname = document.getElementById('firstname').value
@@ -66,10 +111,9 @@ function sendOrder() {
   )) {
     alert("Veuillez remplir les champs correctements avant de procéder au paiement")
     return
-    
   }
 
-  const product = Object.values(cart.product).map((product) => {
+  const products = Object.values(Cart.products).map((product) => {
     return product._id
   })
 
@@ -81,7 +125,7 @@ function sendOrder() {
       city: city,
       email: email,
     },
-    product: product,
+    products: products,
   }
 
   const requestOptions = {
@@ -89,16 +133,41 @@ function sendOrder() {
     body: JSON.stringify(order),
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
   }
-
-
+ 
   fetch(`http://localhost:3000/api/camera/order`, requestOptions)
     .then((response) => response.json())
     .then((json) => {
       console.log(json)
       localStorage.removeItem('cart', JSON.stringify(cart))
-      window.location.href("confirmation.html")
+      window.location.href = `${window.location.origin}/confirmation.html?orderId=${json.orderId}`
     })
     .catch(() => {
       alert(error)
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
