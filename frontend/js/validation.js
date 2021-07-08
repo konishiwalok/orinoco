2893518f0cac1dd3ec8720e192685f95bf77ea31
angie <sophie-girard91@outlook.fr>
2021-07-08 20:12:21 +0200

    regler la responsive

--- frontend/js/validation.js
+++ frontend/js/validation.js
@@ -1,10 +1,5 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
-// const btnCart = document.getElementById('confirmPurchase')// copiado
-
-
-// ..............................
-
 const btnCart = document.getElementById('confirmPurchase')
 
 
@@ -13,8 +8,8 @@ const expresiones = {
 	lastname: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	zipcode: /[0-9]{5}(-[0-9]{4})?/,
-	city: /^[a-zA-Z0-9\_\-]{4,16}$/,
-	adress: /^[a-zA-Z0-9\s\,\''\-]*$/,
+	city: /^[a-zA-ZÀ-ÿ0-9\_\s\,\-]{4,16}$/,
+	adress: /^[a-zA-ZÀ-ÿ0-9\s\,\''\-]*$/,
 }
 
 const campos = {
@@ -25,7 +20,6 @@ const campos = {
 	city: false,
 	adress:false,
 }
-
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
@@ -74,87 +68,23 @@ inputs.forEach((input) => {
 	//input.addEventListener('blur', validarFormulario);
 });
 
-formulario.addEventListener('submit', e => {
+btnCart.addEventListener('click', e => {
   //console.log(e.target.id === 'confirmPurchase');
 
   console.log(e)
   console.log('entro1')
-  p();
-  //sendInfo(e);
+  sendInfo(e);
   console.log('entro2')
 })
 
-
-const p = () => {
-  const firstname = document.getElementById('firstname').value
-    const lastname = document.getElementById('lastname').value
-    const adress = document.getElementById('adress').value
-    const zipcode = document.getElementById('zipcode').value
-    const email = document.getElementById('email').value
-    const city = document.getElementById('city').value
-
-    const lsc = JSON.parse(localStorage.getItem('cart'))
-    const products = Object.values(lsc).map((product) => {
-      return product.id
-    })
-
-    console.log(lsc);
-    console.log(products);
-
-    const order = {
-      contact: {
-        firstName: firstname,
-        lastName: lastname,
-        address: adress + ' ' + zipcode,
-        city: city,
-        email: email,
-      },
-      products: products,
-    }
-
-    console.log(order)
-
-    const url = 'http://localhost:3000/api/cameras/order';
-    const requestOptions = {
-      method: 'POST',
-      body: JSON.stringify(order),
-      headers: { 'Content-Type': 'application/json; charset=utf-8' },
-    }
-
-    const totalPrice = document.getElementById('total-price').innerHTML
-    
-
-    fetch(url, requestOptions)
-      .then((response) => response.json())
-      .then((json) => {
-        
-        const info = {
-          firstName: json.contact.firstName,
-          lastName: json.contact.lastName,
-          address: json.contact.address,
-          city: json.contact.city,
-          price: totalPrice,
-        }
-        console.log(info)
-        //localStorage.removeItem('cart', JSON.stringify(cart))
-        localStorage.setItem('dUser', JSON.stringify(info));
-        window.location.href = `${window.location.origin}/frontend/confirmation.html?orderId=${json.orderId}`
-      })
-      .catch(() => {
-        alert(error)
-      })
-  
-
-}
-
 const sendInfo = e => {
-  console.log(e.target);
-  console.log(e.target.classList.contains('confirmPurchase'));
-  console.log('====================================');
-      console.log('inicio');
-      console.log('====================================');
+  //console.log(e.target);
+  //console.log(e.target.classList.contains('add-cart'));
+  // console.log('====================================');
+  //     console.log('inicio');
+  //     console.log('====================================');
 
-   
+  if (e.target.classList.contains('confirmPurchase')) {
     const firstname = document.getElementById('firstname').value
     const lastname = document.getElementById('lastname').value
     const adress = document.getElementById('adress').value
@@ -162,19 +92,30 @@ const sendInfo = e => {
     const email = document.getElementById('email').value
     const city = document.getElementById('city').value
 
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    const zipcodeRegex = /[0-9]{5}(-[0-9]{4})?/
 
-    console.log('====================================');
-    console.log('ultimo');
-    console.log('====================================');
+    if (!(
+        firstname.length > 1
+        && lastname.length > 1
+        && emailRegex.test(email)
+        && adress.length > 6
+        && zipcodeRegex.test(zipcode)
+        && city.length > 1
+    )) {
+    //     console.log('====================================');
+    // console.log('fallo');
+    // console.log('====================================');
+        alert("Veuillez remplir les champs correctements avant de procéder au paiement")
+    }
+    // console.log('====================================');
+    // console.log('ultimo');
+    // console.log('====================================');
 
     const lsc = JSON.parse(localStorage.getItem('cart'))
     const products = Object.values(lsc).map((product) => {
       return product.id
     })
-
-    console.log('====================================');
-    console.log('22');
-    console.log('====================================');
 
     const order = {
       contact: {
@@ -186,8 +127,6 @@ const sendInfo = e => {
       },
       products: products,
     }
-
-    console.log(order)
 
     const url = 'http://localhost:3000/api/cameras/order';
     const requestOptions = {
@@ -203,6 +142,7 @@ const sendInfo = e => {
       .then((response) => response.json())
       .then((json) => {
         
+
         const info = {
           firstName: json.contact.firstName,
           lastName: json.contact.lastName,
@@ -218,18 +158,6 @@ const sendInfo = e => {
       .catch(() => {
         alert(error)
       })
-   
+  }
   e.stopPropagation();
 }
-
-
-
-
-
-
-
-
-
-
-
-
